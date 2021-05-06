fca128108307faf62bc083ef6cbcdbf73494dc43
CleanMachine1 <78213164+CleanMachine1@users.noreply.github.com>
2021-05-06 23:56:58 +0100

    Added DollarSkip.c & makefile

---
+++ dollarskip.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <string.h>
+char command[4096];
+*buff = 0;
+int main(int argc, char **argv)
+{
+for (int i=1; i<argc; i++){
+    strcat(strcat(command,argv[i])," ");
+    
+}
+command[strlen(command) - 1] = '\0';
+
+system(command);
+}
---
+++ makefile
@@ -0,0 +1,8 @@
+make :
+	gcc dollarskip.c -o $.temp
+install :
+	cp \temp /usr/bin/\$
+uninstall :
+	rm /usr/bin/\$
+clean :
+	rm temp

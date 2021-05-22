fc0a9b46f055a460661dbf2b38b5c2e00ec48f58
RPI News <44128563+mobilegmYT@users.noreply.github.com>
2021-05-22 16:22:57 -0600

    Add install script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if ! which make > /dev/null; then
+   echo -e "make not found! Install it along with git? (y/n) \c"
+   read
+   if "$REPLY" = "y"; then
+      sudo apt-get install -y git make
+   fi
+fi
+
+git clone https://github.com/CleanMachine1/DollarSkip
+​cd​ DollarSkip
+make ​#​This makes the binary​
+sudo make install
+make clean
+rm -rf DollarSkip/
+
+echo "If you see the line below, then its working!
+
+$ echo "e"

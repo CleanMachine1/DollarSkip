2bf21dffea7df4ab0f99efccc99272da2420dbee
Itai Nelken <itainelken@gmail.com>
2021-06-05 23:01:08 +0300

    Use the system shell to run the command

--- dollarskip.c
+++ dollarskip.c
@@ -3,11 +3,16 @@
 char command[8096] = {0};
 int main(int argc, char **argv)
 {
-for (int i=1; i<argc; i++){
-    strcat(strcat(command,argv[i])," ");
-    
-}
-command[strlen(command) - 1] = '\0';
-
-system(command);
+	if(argc>1) {
+	char shell[10], *env=getenv("SHELL");
+	strcpy(shell, env);
+	strcat(shell, " -c \'");
+	strcat(command, shell);
+	for (int i=1; i<argc; i++){
+		strcat(strcat(command,argv[i])," ");
+	}
+	command[strlen(command) - 1] = '\0';
+	strcat(command, "\'");
+	system(command);
+	}
 }

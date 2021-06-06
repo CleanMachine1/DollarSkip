315ac82100d716965a42142f58c39d484be2e94b
Itai Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2021-06-06 18:21:47 +0300

    Run the command using users default shell (#13)
    Signed-off-by: Itai Nelken <itainelken@gmail.com>

--- dollarskip.c
+++ dollarskip.c
@@ -1,13 +1,25 @@
 #include <stdlib.h>
 #include <string.h>
-char command[8096] = {0};
 int main(int argc, char **argv)
 {
-for (int i=1; i<argc; i++){
-    strcat(strcat(command,argv[i])," ");
-    
-}
-command[strlen(command) - 1] = '\0';
-
-system(command);
+	char command[8096] = {0};
+	if(argc>1) {
+		char shell[10], *env=getenv("SHELL"); //get the env var 'SHELL'
+		if(strcmp(env, "")) { //if env != ""
+			strcpy(shell, env); //copy env to shell
+			strcat(shell, " -c \'"); //add " -c '"
+			strcat(command, shell); //add it to command
+			for (int i=1; i<argc; i++){
+				strcat(strcat(command,argv[i])," ");
+			} //add all the command (cmd args) to command
+			command[strlen(command) - 1] = '\0';
+			strcat(command, "\'");// add "'"
+		} else {
+			for (int i=1; i<argc; i++){
+				strcat(strcat(command,argv[i])," ");
+			}
+			command[strlen(command) - 1] = '\0';
+		}
+		system(command);// run the command
+	}
 }

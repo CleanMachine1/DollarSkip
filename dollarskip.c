f759b53c1994393b56abec15ff28373d2061c875
CleanMachine1 <78213164+CleanMachine1@users.noreply.github.com>
2021-06-27 10:29:26 +0100

    Revert "Moving @Itai-Nelken commit to main (See for more information)  (#14)"
    This reverts commit c6e8dd4a0ff4a4237ee5a84871f8ea99590bb767.

--- dollarskip.c
+++ dollarskip.c
@@ -1,25 +1,13 @@
 #include <stdlib.h>
 #include <string.h>
+char command[8096] = {0};
 int main(int argc, char **argv)
 {
-	char command[8096] = {0};
-	if(argc>1) {
-		char shell[128] = {0}, *env=getenv("SHELL"); //get the env var 'SHELL'
-		if(strcmp(env, "")) { //if env != ""
-			strcpy(shell, env); //copy env to shell
-			strcat(shell, " -c \'"); //add " -c '"
-			strcat(command, shell); //add it to command
-			for (int i=1; i<argc; i++){
-				strcat(strcat(command,argv[i])," ");
-			} //add all the command (cmd args) to command
-			command[strlen(command) - 1] = '\0';
-			strcat(command, "\'");// add "'"
-		} else {
-			for (int i=1; i<argc; i++){
-				strcat(strcat(command,argv[i])," ");
-			}
-			command[strlen(command) - 1] = '\0';
-		}
-		system(command);// run the command
-	}
+for (int i=1; i<argc; i++){
+    strcat(strcat(command,argv[i])," ");
+    
+}
+command[strlen(command) - 1] = '\0';
+
+system(command);
 }

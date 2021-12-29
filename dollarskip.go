d42d9bd0f7295035f538560aa211898322da56b2
CleanMachine1 <78213164+CleanMachine1@users.noreply.github.com>
2021-12-29 13:26:13 +0100

    Added a go version of dollarskip

---
+++ dollarskip.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"os"      // for capturing argv
+	"os/exec" // for running the commands
+	"strings" // editing the strings such as removing square brackets and adding spaces
+)
+
+func main() {
+	input := os.Args[1:] // collects given arguments and 1: removes the binary name
+
+	var1 := strings.Join(input, " ") // convert to string rather than type string[] from args
+	var2 := strings.Trim(var1, "[]") // Remove square brackets from using 1: earlier
+
+	cmd := exec.Command(`bash`, `-c`, var2) // execute the command
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	cmd.Run() 
+
+}
